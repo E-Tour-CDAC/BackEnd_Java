f01932f16a47a9156a30dce03f6b1e5b17bd0df5
Jishan Shaikh <Jishanshaikh32622@gmail.com>
2026-01-21 14:04:11 +0530

    home and passanger

--- BackEnd_Java/src/main/java/com/example/repositories/PassengerRepository.java
+++ BackEnd_Java/src/main/java/com/example/repositories/PassengerRepository.java
@@ -1,7 +1,12 @@
 package com.example.repositories;
 
 import com.example.entities.Passenger;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PassengerRepository extends JpaRepository<Passenger,Integer> {
+	
+	List<Passenger> findByBookingId(Integer bookingId);
 }
