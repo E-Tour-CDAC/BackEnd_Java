f3ccd3393874bb6bcd13cdc9d69d070db5a7686f
p18an1v <pranavmhargude@gmail.com>
2026-01-21 20:06:51 +0530

    u: update customer - dto,entite,model

--- BackEnd_Java/src/main/java/com/example/entities/CustomerMaster.java
+++ BackEnd_Java/src/main/java/com/example/entities/CustomerMaster.java
@@ -15,12 +15,8 @@ import java.util.Set;
 
 @Entity
 @Table(name = "customer_master")
-<<<<<<< HEAD
 @Getter
 @Setter
-=======
-
->>>>>>> 72611e12090a56c25edf5241cb23cf14338af9c0
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerMaster {
@@ -54,9 +50,6 @@ public class CustomerMaster {
     @Column(name = "customer_role", nullable = false)
     private CustomerRole customerRole;
 
-<<<<<<< HEAD
-    public Integer getId() {
-=======
     @ColumnDefault("'LOCAL'")
     @Lob
     @Enumerated(EnumType.STRING)
@@ -69,111 +62,4 @@ public class CustomerMaster {
 
     @OneToMany(mappedBy = "customer")
     private Set<BookingHeader> bookingHeaders = new LinkedHashSet<>();
-
-	public Integer getId() {
->>>>>>> 72611e12090a56c25edf5241cb23cf14338af9c0
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public CustomerRole getCustomerRole() {
-		return customerRole;
-	}
-
-	public void setCustomerRole(CustomerRole customerRole) {
-		this.customerRole = customerRole;
-	}
-
-	public AuthProvider getAuthProvider() {
-		return authProvider;
-	}
-
-	public void setAuthProvider(AuthProvider authProvider) {
-		this.authProvider = authProvider;
-	}
-
-	public Boolean getProfileCompleted() {
-		return profileCompleted;
-	}
-
-	public void setProfileCompleted(Boolean profileCompleted) {
-		this.profileCompleted = profileCompleted;
-	}
-
-	public Set<BookingHeader> getBookingHeaders() {
-		return bookingHeaders;
-	}
-
-	public void setBookingHeaders(Set<BookingHeader> bookingHeaders) {
-		this.bookingHeaders = bookingHeaders;
-	}
-<<<<<<< HEAD
-
-	@ColumnDefault("'LOCAL'")
-    @Lob
-    @Enumerated(EnumType.STRING)
-    @Column(name = "auth_provider", nullable = false)
-    private AuthProvider authProvider;
-
-    @ColumnDefault("0")
-    @Column(name = "profile_completed", nullable = false)
-    private Boolean profileCompleted;
-
-    @OneToMany(mappedBy = "customer")
-    private Set<BookingHeader> bookingHeaders = new LinkedHashSet<>();
-=======
-    
-    
->>>>>>> 72611e12090a56c25edf5241cb23cf14338af9c0
 }
