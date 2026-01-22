2c51e3585f530bde42f9389de75c616948783d04
mohitjp-dev <patilmj.123@gmail.com>
2026-01-22 16:02:38 +0530

    changes to jishan home controller

--- BackEnd_Java/src/main/java/com/example/controller/HomeController.java
+++ BackEnd_Java/src/main/java/com/example/controller/HomeController.java
@@ -9,22 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.dto.CategoryDTO;
 import com.example.dto.TourGuideDTO;
 import com.example.services.CategoryService;
-<<<<<<< HEAD
-import com.example.services.TourGuideServices;
-import com.example.services.TourService;
-=======
 
->>>>>>> c468b0a0cce32968be2185e99f4a8e860809e298
 
 @RestController
 @RequestMapping("/home")
 public class HomeController {
 
 	private final CategoryService categoryService;
-<<<<<<< HEAD
-	private final TourService tourService;
-=======
->>>>>>> c468b0a0cce32968be2185e99f4a8e860809e298
+
 
 
 	public HomeController(CategoryService categoryService) {
