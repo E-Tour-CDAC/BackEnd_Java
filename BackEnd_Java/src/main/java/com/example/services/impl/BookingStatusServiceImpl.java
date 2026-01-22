55fe5983b2b17ae84c0f6ab3e870f3de190ba074
mohitjp-dev <patilmj.123@gmail.com>
2026-01-22 14:59:04 +0530

    bookingstatus

--- BackEnd_Java/src/main/java/com/example/services/impl/BookingStatusServiceImpl.java
+++ BackEnd_Java/src/main/java/com/example/services/impl/BookingStatusServiceImpl.java
@@ -1,5 +1,32 @@
 package com.example.services.impl;
 
-public class BookingStatusServiceImpl {
+import java.util.List;
+import java.util.stream.Collectors;
 
-}
+import com.example.services.BookingStatusService;
+import org.springframework.stereotype.Service;
+
+import com.example.dto.BookingStatusDTO;
+import com.example.entities.BookingStatusMaster;
+import com.example.repositories.BookingStatusRepository;
+
+@Service
+public class BookingStatusServiceImpl  implements BookingStatusService {
+
+    private final BookingStatusRepository statusRepository;
+
+    public BookingStatusServiceImpl(BookingStatusRepository statusRepository) {
+        this.statusRepository = statusRepository;
+    }
+    @Override
+    public List<BookingStatusDTO> getAllStatuses(){
+
+        return statusRepository.findAll()
+                .stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public BookingStatusDTO getStatusById(Integer statusId) {
+        BookingStatusMaster status = statusRepository.findById(statusId)
+
