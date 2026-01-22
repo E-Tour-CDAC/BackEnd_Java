e6abb928dfc528bfe7bc45d8208ac7a2c2cf9e7a
Jishan Shaikh <Jishanshaikh32622@gmail.com>
2026-01-22 16:01:58 +0530

    updated home controller

--- BackEnd_Java/src/main/java/com/example/controller/HomeController.java
+++ BackEnd_Java/src/main/java/com/example/controller/HomeController.java
@@ -9,23 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.dto.CategoryDTO;
 import com.example.dto.TourGuideDTO;
 import com.example.services.CategoryService;
-<<<<<<< HEAD
-import com.example.services.TourGuideServices;
+//import com.example.services.TourGuideServices;
 import com.example.services.TourService;
-=======
 
->>>>>>> c468b0a0cce32968be2185e99f4a8e860809e298
 
 @RestController
 @RequestMapping("/home")
 public class HomeController {
 
 	private final CategoryService categoryService;
-<<<<<<< HEAD
-	private final TourService tourService;
-=======
->>>>>>> c468b0a0cce32968be2185e99f4a8e860809e298
 
+//	private final TourService tourService;
 
 	public HomeController(CategoryService categoryService) {
 		this.categoryService = categoryService;
